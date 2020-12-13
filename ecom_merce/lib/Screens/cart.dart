ad1fbb29d293195d04f609489cac55945ef93659
Askydev <devashishsharma2016@gmail.com>
2020-12-13 15:21:27 +0530

    cart page added

--- ecom_merce/lib/Screens/cart.dart
+++ ecom_merce/lib/Screens/cart.dart
@@ -1,17 +1,107 @@
 import 'package:flutter/material.dart';
 
+
+class CartScreen extends StatefulWidget {
+
+  final double price;
+  final String name;
+  final String image;
+
+  CartScreen({this.name,this.price,this.image});
+
+  @override
+  _CartScreenState createState() => _CartScreenState();
+}
+
 final TextStyle mystyle = TextStyle(
     fontWeight: FontWeight.bold,
     fontSize: 20
 );
 
-class CartScreen extends StatefulWidget {
-  @override
-  _CartScreenState createState() => _CartScreenState();
-}
-
 class _CartScreenState extends State<CartScreen> {
   int count=1;
+
+  Widget _buildCartProduct(){
+    return Container(
+      height: 180,
+      width: double.infinity,
+      child: Card(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                Container(
+                  height: 130,
+                  width: 130,
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                        fit: BoxFit.fill,
+                        image: AssetImage("images/${widget.image}"),
+                      )
+                  ),
+                ),
+                Container(
+                  height: 130,
+                  width: 260,
+                  child: ListTile(
+
+                    title: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(widget.name),
+                        Text("Electronics"),
+                        Text("\$ ${widget.price.toString()}",
+                          style: TextStyle(
+                              color: Colors.black54,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        Container(
+                          height: 30,
+                          width: 110,
+                          color: Colors.black12,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              GestureDetector(
+                                child: Icon(Icons.remove),
+                                onTap: (){
+                                  setState(() {
+                                    if(count>1){
+                                      count--;
+                                    }
+                                  }
+                                  );
+                                },
+                              ),
+                              Text(
+                                  count.toString(),
+                                  style: TextStyle(
+                                      fontSize: 18
+                                  )
+                              ),
+                              GestureDetector(
+                                child: Icon(Icons.add),
+                                onTap: (){
+                                  count++;
+                                },
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -37,84 +127,14 @@ class _CartScreenState extends State<CartScreen> {
           ),
         ],
       ),
-      body: Container(
-        height: 180,
-        width: double.infinity,
-        child: Card(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                children: [
-                  Container(
-                    height: 130,
-                    width: 130,
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        fit: BoxFit.fill,
-                        image: AssetImage("images/galaxy.png"),
-                      )
-                    ),
-                  ),
-                  Container(
-                    height: 130,
-                    width: 260,
-                    child: ListTile(
-
-                      title: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text("Samsung Galaxy Gear-S2"),
-                          Text("Electronics"),
-                          Text("\$ 30.0",
-                            style: TextStyle(
-                                color: Colors.black54,
-                                fontWeight: FontWeight.bold),
-                          ),
-                          Container(
-                            height: 30,
-                            width: 110,
-                            
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              children: [
-                                GestureDetector(
-                                  child: Icon(Icons.remove),
-                                  onTap: (){
-                                    setState(() {
-                                      if(count>1){
-                                        count--;
-                                      }
-                                    }
-                                    );
-                                  },
-                                ),
-                                Text(
-                                    count.toString(),
-                                    style: TextStyle(
-                                        fontSize: 18
-                                    )
-                                ),
-                                GestureDetector(
-                                  child: Icon(Icons.add),
-                                  onTap: (){
-                                    count++;
-                                  },
-                                ),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ),
+      body: ListView(
+        children: [
+          _buildCartProduct(),
+          _buildCartProduct(),
+          _buildCartProduct(),
+          _buildCartProduct(),
+          _buildCartProduct()
+        ],
       ),
     );
   }
