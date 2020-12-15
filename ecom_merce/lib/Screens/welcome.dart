d240423a5234c10e335ac3db54ebab7a586f31dc
Askydev <devashishsharma2016@gmail.com>
2020-12-15 11:36:06 +0530

    Welcome Screen started

--- ecom_merce/lib/Screens/welcome.dart
+++ ecom_merce/lib/Screens/welcome.dart
@@ -4,7 +4,26 @@ class WelCome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      body: Center(
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                height: 400,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    fit: BoxFit.cover,
+                      image: AssetImage("images/welcome.png"))
+                ),
+              ),
+              Text("Welcome"),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
