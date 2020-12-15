b55219c90a6c5eb7e22bb7786b86e3eddd5efba2
Askydev <devashishsharma2016@gmail.com>
2020-12-15 12:18:17 +0530

    Welcome Screen started

--- ecom_merce/lib/Screens/welcome.dart
+++ ecom_merce/lib/Screens/welcome.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
 
 class WelCome extends StatelessWidget {
   @override
@@ -19,7 +22,42 @@ class WelCome extends StatelessWidget {
                       image: AssetImage("images/welcome.png"))
                 ),
               ),
-              Text("Welcome"),
+              Text("Welcome",style: TextStyle(fontWeight: FontWeight.w300,fontSize: 22),),
+              SizedBox(
+                height: 30,
+              ),
+              Container(
+                height: 45,
+                width: 250,
+                child: RaisedButton(
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+                  color: Colors.cyan[200],
+                  elevation: 10.0,
+                  child: Text("Sign Up",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18
+                  ),
+                  ),
+                  onPressed: (){},
+                ),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Row(
+                // crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Already have an account? \t\t",style: TextStyle(fontSize: 15),),
+                  GestureDetector(
+                    child: Text("Login", style: TextStyle(color: Colors.green,fontSize: 17),),
+                    onTap: (){
+                      
+                    },
+                  ),
+                ],
+              ),
             ],
           ),
         ),
