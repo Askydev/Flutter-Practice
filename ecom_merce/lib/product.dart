f2ab6d7dc7838b8024eab7150148b2ded955c829
Askydev <devashishsharma2016@gmail.com>
2021-01-26 22:51:49 +0530

    product model

--- ecom_merce/lib/product.dart
+++ ecom_merce/lib/product.dart
@@ -1,1 +1,7 @@
 import 'package:flutter/material.dart';
+class Product{
+  final String name;
+  final String image;
+  final double price;
+  Product({@required this.name,@required this.image,@required this.price});
+}
