fdb45cc8899d850a99dab83b67f45be6d265e322
Askydev <devashishsharma2016@gmail.com>
2021-02-19 20:17:47 +0530

    recent chats....

--- chat_app_ui/lib/widgets/Recent_chats.dart
+++ chat_app_ui/lib/widgets/Recent_chats.dart
@@ -12,55 +12,64 @@ class RecentChats extends StatelessWidget {
             topRight: Radius.circular(30.0),
           ),
         ),
-        child: ListView.builder(
-            itemCount: chats.length,
-            itemBuilder: (BuildContext context, int index){
-              final Message chat = chats[index];
-              return Row(
-                children: [
-                  Row(
-                    children: [
-                      CircleAvatar(
-                        radius: 35.0,
-                        backgroundImage: AssetImage(chat.sender.imageUrl),
-                      ),
-                      Container(
-                        width: MediaQuery.of(context).size.width*0.65,
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                                chat.sender.name,
-                              style: TextStyle(
-                                color: Colors.grey,
-                                fontSize: 15.0,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            Text(
-                                chat.text,
-                              style: TextStyle(
-                                color: Colors.blueGrey,
-                                fontSize: 15.0,
-                                fontWeight: FontWeight.w600,
-                              ),
-                              overflow: TextOverflow.ellipsis,
-                            ),
-                          ],
+        child: ClipRRect(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(30.0),
+            topRight: Radius.circular(30.0),
+          ),
+          child: ListView.builder(
+              itemCount: chats.length,
+              itemBuilder: (BuildContext context, int index){
+                final Message chat = chats[index];
+                return Row(
+                  children: [
+                    Row(
+                      children: [
+                        CircleAvatar(
+                          radius: 35.0,
+                          backgroundImage: AssetImage(chat.sender.imageUrl),
                         ),
-                      ),
-                    ],
-                  ),
-                  Column(
-                    children: [
-                      Text(
-                        chat.time
-                      ),
-                    ],
-                  ),
-                ],
-              );
-            }
+                        SizedBox(
+                          width: 10.0,
+                        ),
+                        Container(
+                          width: MediaQuery.of(context).size.width*0.65,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                  chat.sender.name,
+                                style: TextStyle(
+                                  color: Colors.grey,
+                                  fontSize: 15.0,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text(
+                                  chat.text,
+                                style: TextStyle(
+                                  color: Colors.blueGrey,
+                                  fontSize: 15.0,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Text(
+                          chat.time
+                        ),
+                      ],
+                    ),
+                  ],
+                );
+              }
+          ),
         ),
       ),
     );
