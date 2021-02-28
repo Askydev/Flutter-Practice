791f859d67478d49f81b320ecd7a33328b5590dd
Askydev <devashishsharma2016@gmail.com>
2021-03-01 00:21:51 +0530

    ChatScreen

--- chat_app_ui/lib/screens/Chat_screen.dart
+++ chat_app_ui/lib/screens/Chat_screen.dart
@@ -1,5 +1,6 @@
 import 'package:chat_app_ui/models/message_model.dart';
 import 'package:chat_app_ui/models/usermodel.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatefulWidget {
@@ -13,11 +14,69 @@ class ChatScreen extends StatefulWidget {
 
 class _ChatScreenState extends State<ChatScreen> {
   _buildMessage(Message message, bool isMe) {
-    return Container(
-      color: Color(0xFFFFEFEE),
-      child: Text(
-        message.text,
+    final Container msg = Container(
+      margin: isMe
+          ? EdgeInsets.only(top: 8.0, bottom: 8.0, left: 80.0)
+          : EdgeInsets.only(
+              top: 8.0,
+              bottom: 8.0,
+            ),
+      padding: EdgeInsets.symmetric(horizontal: 25.0, vertical: 15.0),
+      width: MediaQuery.of(context).size.width * 0.75,
+      decoration: BoxDecoration(
+        color: isMe ? Theme.of(context).accentColor : Color(0xFFFFEFEE),
+        borderRadius: isMe
+            ? BorderRadius.only(
+                topLeft: Radius.circular(15.0),
+                bottomLeft: Radius.circular(15.0))
+            : BorderRadius.only(
+                topRight: Radius.circular(15.0),
+                bottomRight: Radius.circular(15.0)),
       ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            message.time,
+            style: TextStyle(
+              color: Colors.blueGrey,
+              fontSize: 16.0,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          SizedBox(
+            height: 8.0,
+          ),
+          Text(
+            message.text,
+            style: TextStyle(
+              color: Colors.blueGrey,
+              fontSize: 16.0,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+        ],
+      ),
+    );
+    if (isMe) {
+      return msg;
+    }
+    return Row(
+      children: [
+        msg,
+        !isMe
+            ? IconButton(
+                icon: message.isLiked
+                    ? Icon(Icons.favorite)
+                    : Icon(Icons.favorite_border),
+                iconSize: 30.0,
+                color: message.isLiked
+                    ? Theme.of(context).primaryColor
+                    : Colors.blueGrey,
+                onPressed: () {},
+              )
+            : SizedBox.shrink(),
+      ],
     );
   }
 
@@ -54,6 +113,7 @@ class _ChatScreenState extends State<ChatScreen> {
                     topRight: Radius.circular(30.0),
                     topLeft: Radius.circular(30.0)),
                 child: ListView.builder(
+                  reverse: true,
                   padding: EdgeInsets.only(top: 15.0),
                   itemCount: messages.length,
                   itemBuilder: (BuildContext context, int index) {
